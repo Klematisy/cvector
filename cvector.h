f6493ae86947eb9ab2bff0e46f3279d9faa82d13
Klematisy <101270054+Klematisy@users.noreply.github.com>
2025-05-11 22:49:47 +0300

    fix

--- cvector.h
+++ cvector.h
@@ -34,14 +34,14 @@ static void cv_clear(Vector *vector);
 
 /*--------------------------------IMPLEMENTATION--------------------------------*/
 
-void cv_create(Vector *vector, u_int32_t size) {
+static void cv_create(Vector *vector, u_int32_t size) {
     vector->data = malloc(size * start_size);
     vector->type_size = size;
     vector->allocated_size = 2;
     vector->size = 0;
 }
 
-void cv_set_size(Vector *vector, uint32_t size) {
+static void cv_set_size(Vector *vector, uint32_t size) {
     void* new_data_section = realloc(vector->data, vector->allocated_size * size * vector->type_size);
     if (new_data_section) {
         vector->data = new_data_section;
@@ -51,7 +51,7 @@ void cv_set_size(Vector *vector, uint32_t size) {
     }
 }
 
-void cv_add_element_(Vector *vector, void *element) {
+static void cv_add_element_(Vector *vector, void *element) {
     if (vector->size == vector->allocated_size) {
         void* new_data_section = realloc(vector->data, vector->allocated_size * 2 * vector->type_size);
         if (new_data_section) {
@@ -68,18 +68,18 @@ void cv_add_element_(Vector *vector, void *element) {
     vector->size++;
 }
 
-void cv_delete(Vector *vector) {
+static void cv_delete(Vector *vector) {
     free(vector->data);
 }
 
-void cv_pop_back(Vector *vector) {
+static void cv_pop_back(Vector *vector) {
     if (vector->size > 0) {
         vector->size--;
         memset(vector->data + vector->size * vector->type_size, 0, vector->type_size);
     }
 }
 
-void cv_erase(Vector *vector, uint32_t i) {
+static void cv_erase(Vector *vector, uint32_t i) {
     if (i < vector->size) {
         memcpy(vector->data + i * vector->type_size,
                vector->data + (i + 1) * vector->type_size,
@@ -90,7 +90,7 @@ void cv_erase(Vector *vector, uint32_t i) {
     }
 }
 
-void cv_clear(Vector *vector) {
+static void cv_clear(Vector *vector) {
     memset(vector->data, 0, vector->size * vector->type_size);
     vector->size = 0;
 }
